b50f009a7e8dc56715edb5783984ee067a6d605d
Ali Emre Gülcü <aliemre.sirius@gmail.com>
2020-06-11 22:32:39 +0300

    Included GLM, created vertex buffer with staging memory and implemented FPS counter

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -7,9 +7,22 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <chrono>
+#include <glm/glm.hpp>
 
 #define TAG "MoleculeVRNativeMain"
 #define LOG(...) ((void)__android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__))
+
+struct Vertex {
+    glm::vec2 pos;
+    glm::vec3 color;
+};
+
+const std::vector<Vertex> vertices = {
+        {{0.0f,  -0.5f}, {1.0f, 1.0f, 1.0f}},
+        {{0.5f,  0.5f},  {0.0f, 1.0f, 0.0f}},
+        {{-0.5f, 0.5f},  {0.0f, 0.0f, 1.0f}}
+};
 
 android_app *app;
 VkInstance instance;
@@ -18,9 +31,10 @@ VkSurfaceKHR surface;
 VkPhysicalDevice physicalDevice;
 VkDevice device;
 VkQueue queue;
+VkCommandPool commandPool;
 VkSwapchainKHR swapchain;
 VkExtent2D swapchainExtent;
-uint32_t imageCount, currentFrame;
+uint32_t imageCount, currentImage;
 std::vector<VkImage> swapchainImages;
 std::vector<VkImageView> swapchainImageViews;
 VkShaderModule vertexShader, fragmentShader;
@@ -28,7 +42,8 @@ VkRenderPass renderPass;
 VkPipelineLayout pipelineLayout;
 VkPipeline graphicsPipeline;
 std::vector<VkFramebuffer> framebuffers;
-VkCommandPool commandPool;
+VkBuffer vertexBuffer;
+VkDeviceMemory vertexMemory;
 std::vector<VkCommandBuffer> commandBuffers;
 std::vector<VkFence> frameFences, orderFences;
 std::vector<VkSemaphore> availableSemaphores, finishedSemaphores;
@@ -37,6 +52,7 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
         VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
         VkDebugUtilsMessageTypeFlagsEXT messageType,
         const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData, void *pUserData) {
+    (void) pUserData;
     int severity;
     const char *type;
     if (messageSeverity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT)
@@ -133,8 +149,13 @@ void pickDevice() {
     deviceInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
     deviceInfo.ppEnabledExtensionNames = deviceExtensions.data();
 
+    VkCommandPoolCreateInfo poolInfo{};
+    poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    poolInfo.queueFamilyIndex = 0;
+
     vkCreateDevice(physicalDevice, &deviceInfo, nullptr, &device);
     vkGetDeviceQueue(device, 0, 0, &queue);
+    vkCreateCommandPool(device, &poolInfo, nullptr, &commandPool);
 }
 
 void createSwapchain() {
@@ -262,12 +283,27 @@ void createPipeline() {
 
     VkPipelineShaderStageCreateInfo shaderStages[] = {vertexInfo, fragmentInfo};
 
+    VkVertexInputBindingDescription bindingDescription{};
+    bindingDescription.binding = 0;
+    bindingDescription.stride = sizeof(Vertex);
+    bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+    VkVertexInputAttributeDescription attributeDescriptions[2]{};
+    attributeDescriptions[0].binding = 0;
+    attributeDescriptions[0].location = 0;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[0].offset = offsetof(Vertex, pos);
+    attributeDescriptions[1].binding = 0;
+    attributeDescriptions[1].location = 1;
+    attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[1].offset = offsetof(Vertex, color);
+
     VkPipelineVertexInputStateCreateInfo inputInfo{};
     inputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-    inputInfo.vertexBindingDescriptionCount = 0;
-    inputInfo.pVertexBindingDescriptions = nullptr;
-    inputInfo.vertexAttributeDescriptionCount = 0;
-    inputInfo.pVertexAttributeDescriptions = nullptr;
+    inputInfo.vertexBindingDescriptionCount = 1;
+    inputInfo.pVertexBindingDescriptions = &bindingDescription;
+    inputInfo.vertexAttributeDescriptionCount = 2;
+    inputInfo.pVertexAttributeDescriptions = attributeDescriptions;
 
     VkPipelineInputAssemblyStateCreateInfo assemblyInfo{};
     assemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
@@ -385,13 +421,107 @@ void createFramebuffers() {
     }
 }
 
+uint32_t chooseMemoryType(uint32_t filter, VkMemoryPropertyFlags flags) {
+    VkPhysicalDeviceMemoryProperties memoryProperties;
+    vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memoryProperties);
+
+    for (uint32_t index = 0; index < memoryProperties.memoryTypeCount; index++)
+        if ((filter & (1 << index)) &&
+            (memoryProperties.memoryTypes[index].propertyFlags & flags) == flags)
+            return index;
+
+    return UINT_MAX;
+}
+
+void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage,
+                  VkMemoryPropertyFlags properties, VkBuffer &buffer,
+                  VkDeviceMemory &bufferMemory) {
+    VkMemoryRequirements memoryRequirements;
+
+    VkBufferCreateInfo bufferInfo = {};
+    bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    bufferInfo.usage = usage;
+    bufferInfo.size = size;
+
+    vkCreateBuffer(device, &bufferInfo, NULL, &buffer);
+    vkGetBufferMemoryRequirements(device, buffer, &memoryRequirements);
+
+    VkMemoryAllocateInfo allocateInfo = {};
+    allocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocateInfo.allocationSize = memoryRequirements.size;
+    allocateInfo.memoryTypeIndex = chooseMemoryType(memoryRequirements.memoryTypeBits, properties);
+
+    vkAllocateMemory(device, &allocateInfo, NULL, &bufferMemory);
+    vkBindBufferMemory(device, buffer, bufferMemory, 0);
+}
+
+VkCommandBuffer beginSingleTimeCommand() {
+    VkCommandBufferAllocateInfo allocateInfo = {};
+    allocateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocateInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocateInfo.commandPool = commandPool;
+    allocateInfo.commandBufferCount = 1;
+
+    VkCommandBuffer commandBuffer;
+    vkAllocateCommandBuffers(device, &allocateInfo, &commandBuffer);
+
+    VkCommandBufferBeginInfo beginInfo = {};
+    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+    vkBeginCommandBuffer(commandBuffer, &beginInfo);
+    return commandBuffer;
+}
+
+void endSingleTimeCommand(VkCommandBuffer commandBuffer) {
+    vkEndCommandBuffer(commandBuffer);
+
+    VkSubmitInfo submitInfo = {};
+    submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submitInfo.commandBufferCount = 1;
+    submitInfo.pCommandBuffers = &commandBuffer;
+
+    vkQueueSubmit(queue, 1, &submitInfo, VK_NULL_HANDLE);
+    vkQueueWaitIdle(queue);
+    vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
+}
+
+void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size) {
+    VkCommandBuffer commandBuffer = beginSingleTimeCommand();
+
+    VkBufferCopy copyRegion = {};
+    copyRegion.srcOffset = 0;
+    copyRegion.dstOffset = 0;
+    copyRegion.size = size;
+
+    vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
+    endSingleTimeCommand(commandBuffer);
+}
+
+void createVertexBuffer() {
+    VkDeviceSize bufferSize = sizeof(Vertex) * vertices.size();
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingMemory;
+    createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                 VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                 stagingBuffer, stagingMemory);
+
+    void *data;
+    vkMapMemory(device, stagingMemory, 0, bufferSize, 0, &data);
+    memcpy(data, vertices.data(), (size_t) bufferSize);
+    vkUnmapMemory(device, stagingMemory);
+
+    createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+                 VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, vertexBuffer, vertexMemory);
+    copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+    vkDestroyBuffer(device, stagingBuffer, nullptr);
+    vkFreeMemory(device, stagingMemory, nullptr);
+}
+
 void createCommandBuffers() {
-    VkCommandPoolCreateInfo poolInfo{};
-    poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
-    poolInfo.queueFamilyIndex = 0;
-
-    vkCreateCommandPool(device, &poolInfo, nullptr, &commandPool);
-
     VkCommandBufferAllocateInfo allocateInfo{};
     allocateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
     allocateInfo.commandPool = commandPool;
@@ -417,9 +547,13 @@ void createCommandBuffers() {
         renderPassInfo.clearValueCount = 1;
         renderPassInfo.pClearValues = &clearColor;
 
+        VkBuffer vertexBuffers[] = {vertexBuffer};
+        VkDeviceSize offsets[] = {0};
+
         vkBeginCommandBuffer(commandBuffers[i], &beginInfo);
         vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
         vkCmdBindPipeline(commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+        vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
         vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
         vkCmdEndRenderPass(commandBuffers[i]);
         vkEndCommandBuffer(commandBuffers[i]);
@@ -427,7 +561,7 @@ void createCommandBuffers() {
 }
 
 void createSyncObject() {
-    currentFrame = 0;
+    currentImage = 0;
 
     frameFences.resize(imageCount);
     orderFences.resize(imageCount, VK_NULL_HANDLE);
@@ -455,24 +589,25 @@ void setup() {
     createRenderPass();
     createPipeline();
     createFramebuffers();
+    createVertexBuffer();
     createCommandBuffers();
     createSyncObject();
 }
 
 void draw() {
-    vkWaitForFences(device, 1, &frameFences[currentFrame], VK_TRUE, UINT64_MAX);
+    vkWaitForFences(device, 1, &frameFences[currentImage], VK_TRUE, UINT64_MAX);
 
     uint32_t imageIndex;
-    vkAcquireNextImageKHR(device, swapchain, UINT64_MAX, availableSemaphores[currentFrame],
+    vkAcquireNextImageKHR(device, swapchain, UINT64_MAX, availableSemaphores[currentImage],
                           VK_NULL_HANDLE, &imageIndex);
 
     if (orderFences[imageIndex] != VK_NULL_HANDLE)
         vkWaitForFences(device, 1, &orderFences[imageIndex], VK_TRUE, UINT64_MAX);
 
-    orderFences[imageIndex] = frameFences[currentFrame];
+    orderFences[imageIndex] = frameFences[currentImage];
 
-    VkSemaphore waitSemaphores[] = {availableSemaphores[currentFrame]};
-    VkSemaphore signalSemaphores[] = {finishedSemaphores[currentFrame]};
+    VkSemaphore waitSemaphores[] = {availableSemaphores[currentImage]};
+    VkSemaphore signalSemaphores[] = {finishedSemaphores[currentImage]};
     VkPipelineStageFlags waitStages[] = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
 
     VkSubmitInfo submitInfo{};
@@ -485,8 +620,8 @@ void draw() {
     submitInfo.signalSemaphoreCount = 1;
     submitInfo.pSignalSemaphores = signalSemaphores;
 
-    vkResetFences(device, 1, &frameFences[currentFrame]);
-    vkQueueSubmit(queue, 1, &submitInfo, frameFences[currentFrame]);
+    vkResetFences(device, 1, &frameFences[currentImage]);
+    vkQueueSubmit(queue, 1, &submitInfo, frameFences[currentImage]);
 
     VkPresentInfoKHR presentInfo{};
     presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
@@ -498,7 +633,7 @@ void draw() {
     presentInfo.pResults = nullptr;
 
     vkQueuePresentKHR(queue, &presentInfo);
-    currentFrame = (currentFrame + 1) % imageCount;
+    currentImage = (currentImage + 1) % imageCount;
 }
 
 void clear() {
@@ -509,6 +644,8 @@ void clear() {
         vkDestroyFence(device, frameFences[i], nullptr);
     }
     vkDestroyCommandPool(device, commandPool, nullptr);
+    vkDestroyBuffer(device, vertexBuffer, nullptr);
+    vkFreeMemory(device, vertexMemory, nullptr);
     for (auto framebuffer : framebuffers)
         vkDestroyFramebuffer(device, framebuffer, nullptr);
     vkDestroyPipeline(device, graphicsPipeline, nullptr);
@@ -545,11 +682,29 @@ extern "C" void android_main(struct android_app *pApp) {
     int events;
     android_poll_source *pSource;
 
+
+    uint32_t previousFrame = 0, currentFrame = 0;
+    uint64_t currentTime, previousTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now().time_since_epoch()).count();
+
     do {
         if (ALooper_pollAll(0, nullptr, &events, (void **) &pSource) >= 0)
             if (pSource)
                 pSource->process(pApp, pSource);
-        if (pApp->userData)
+
+        if (pApp->userData) {
             draw();
+
+            currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+                    std::chrono::system_clock::now().time_since_epoch()).count();
+
+            if (currentTime - previousTime > 1000) {
+                LOG("FPS: %d\n", currentFrame - previousFrame);
+                previousFrame = currentFrame;
+                previousTime = currentTime;
+            }
+
+            currentFrame++;
+        }
     } while (!pApp->destroyRequested);
 }

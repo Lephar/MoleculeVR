0311a743f431264f221dd927c07ebd0938f49846
Ali Emre Gülcü <aliemre.sirius@gmail.com>
2020-06-12 09:23:27 +0300

    Implemented index and uniform buffers, fixed screen orientation

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -8,21 +8,32 @@
 #include <fstream>
 #include <iostream>
 #include <chrono>
+
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #define TAG "MoleculeVRNativeMain"
 #define LOG(...) ((void)__android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__))
 
 struct Vertex {
-    glm::vec2 pos;
-    glm::vec3 color;
+    glm::vec3 pos;
+    glm::vec3 col;
+};
+
+struct Transform {
+    alignas(16) glm::mat4 model;
+    alignas(16) glm::mat4 view;
+    alignas(16) glm::mat4 proj;
 };
 
 const std::vector<Vertex> vertices = {
-        {{0.0f,  -0.5f}, {1.0f, 1.0f, 1.0f}},
-        {{0.5f,  0.5f},  {0.0f, 1.0f, 0.0f}},
-        {{-0.5f, 0.5f},  {0.0f, 0.0f, 1.0f}}
+        {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}},
+        {{0.5f,  -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}},
+        {{0.5f,  0.5f,  0.0f}, {0.0f, 0.0f, 1.0f}},
+        {{-0.5f, 0.5f,  0.0f}, {1.0f, 1.0f, 1.0f}}
 };
+
+const std::vector<uint16_t> indices = {0, 1, 2, 2, 3, 0};
 
 android_app *app;
 VkInstance instance;
@@ -39,11 +50,16 @@ std::vector<VkImage> swapchainImages;
 std::vector<VkImageView> swapchainImageViews;
 VkShaderModule vertexShader, fragmentShader;
 VkRenderPass renderPass;
+VkDescriptorSetLayout descriptorSetLayout;
 VkPipelineLayout pipelineLayout;
 VkPipeline graphicsPipeline;
 std::vector<VkFramebuffer> framebuffers;
-VkBuffer vertexBuffer;
-VkDeviceMemory vertexMemory;
+VkBuffer vertexBuffer, indexBuffer;
+VkDeviceMemory vertexMemory, indexMemory;
+std::vector<VkBuffer> uniformBuffers;
+std::vector<VkDeviceMemory> uniformMemory;
+VkDescriptorPool descriptorPool;
+std::vector<VkDescriptorSet> descriptorSets;
 std::vector<VkCommandBuffer> commandBuffers;
 std::vector<VkFence> frameFences, orderFences;
 std::vector<VkSemaphore> availableSemaphores, finishedSemaphores;
@@ -177,7 +193,8 @@ void createSwapchain() {
     swapchainInfo.imageExtent = swapchainExtent;
     swapchainInfo.imageArrayLayers = 1;
     swapchainInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
-    swapchainInfo.preTransform = capabilities.currentTransform;
+    //swapchainInfo.preTransform = capabilities.currentTransform;
+    swapchainInfo.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
     swapchainInfo.compositeAlpha = VK_COMPOSITE_ALPHA_INHERIT_BIT_KHR;
     swapchainInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
 
@@ -291,12 +308,12 @@ void createPipeline() {
     VkVertexInputAttributeDescription attributeDescriptions[2]{};
     attributeDescriptions[0].binding = 0;
     attributeDescriptions[0].location = 0;
-    attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
     attributeDescriptions[0].offset = offsetof(Vertex, pos);
     attributeDescriptions[1].binding = 0;
     attributeDescriptions[1].location = 1;
     attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-    attributeDescriptions[1].offset = offsetof(Vertex, color);
+    attributeDescriptions[1].offset = offsetof(Vertex, col);
 
     VkPipelineVertexInputStateCreateInfo inputInfo{};
     inputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
@@ -336,7 +353,7 @@ void createPipeline() {
     rasterizerInfo.polygonMode = VK_POLYGON_MODE_FILL;
     rasterizerInfo.lineWidth = 1.0f;
     rasterizerInfo.cullMode = VK_CULL_MODE_BACK_BIT;
-    rasterizerInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    rasterizerInfo.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
     rasterizerInfo.depthBiasEnable = VK_FALSE;
     rasterizerInfo.depthBiasConstantFactor = 0.0f;
     rasterizerInfo.depthBiasClamp = 0.0f;
@@ -375,10 +392,24 @@ void createPipeline() {
     blendInfo.blendConstants[2] = 0.0f;
     blendInfo.blendConstants[3] = 0.0f;
 
+    VkDescriptorSetLayoutBinding transformLayoutBinding{};
+    transformLayoutBinding.binding = 0;
+    transformLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    transformLayoutBinding.descriptorCount = 1;
+    transformLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+    transformLayoutBinding.pImmutableSamplers = nullptr;
+
+    VkDescriptorSetLayoutCreateInfo descriptorInfo{};
+    descriptorInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    descriptorInfo.bindingCount = 1;
+    descriptorInfo.pBindings = &transformLayoutBinding;
+
+    vkCreateDescriptorSetLayout(device, &descriptorInfo, nullptr, &descriptorSetLayout);
+
     VkPipelineLayoutCreateInfo layoutInfo{};
     layoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-    layoutInfo.setLayoutCount = 0;
-    layoutInfo.pSetLayouts = nullptr;
+    layoutInfo.setLayoutCount = 1;
+    layoutInfo.pSetLayouts = &descriptorSetLayout;
     layoutInfo.pushConstantRangeCount = 0;
     layoutInfo.pPushConstantRanges = nullptr;
 
@@ -433,9 +464,8 @@ uint32_t chooseMemoryType(uint32_t filter, VkMemoryPropertyFlags flags) {
     return UINT_MAX;
 }
 
-void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage,
-                  VkMemoryPropertyFlags properties, VkBuffer &buffer,
-                  VkDeviceMemory &bufferMemory) {
+void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties,
+                  VkBuffer &buffer, VkDeviceMemory &bufferMemory) {
     VkMemoryRequirements memoryRequirements;
 
     VkBufferCreateInfo bufferInfo = {};
@@ -521,6 +551,87 @@ void createVertexBuffer() {
     vkFreeMemory(device, stagingMemory, nullptr);
 }
 
+void createIndexBuffer() {
+    VkDeviceSize bufferSize = sizeof(uint16_t) * indices.size();
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingMemory;
+    createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                 VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                 stagingBuffer, stagingMemory);
+
+    void *data;
+    vkMapMemory(device, stagingMemory, 0, bufferSize, 0, &data);
+    memcpy(data, indices.data(), (size_t) bufferSize);
+    vkUnmapMemory(device, stagingMemory);
+
+    createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+                 VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexMemory);
+    copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+    vkDestroyBuffer(device, stagingBuffer, nullptr);
+    vkFreeMemory(device, stagingMemory, nullptr);
+}
+
+void createUniformBuffers() {
+    VkDeviceSize bufferSize = sizeof(Transform);
+
+    uniformBuffers.resize(imageCount);
+    uniformMemory.resize(imageCount);
+
+    for (size_t i = 0; i < imageCount; i++)
+        createBuffer(bufferSize, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+                     VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                     uniformBuffers[i], uniformMemory[i]);
+}
+
+void createDescriptorPool() {
+    VkDescriptorPoolSize poolSize{};
+    poolSize.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    poolSize.descriptorCount = imageCount;
+
+    VkDescriptorPoolCreateInfo poolInfo{};
+    poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    poolInfo.poolSizeCount = 1;
+    poolInfo.pPoolSizes = &poolSize;
+    poolInfo.maxSets = imageCount;
+
+    vkCreateDescriptorPool(device, &poolInfo, nullptr, &descriptorPool);
+}
+
+void createDescriptorSets() {
+    std::vector<VkDescriptorSetLayout> layouts(imageCount, descriptorSetLayout);
+
+    VkDescriptorSetAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    allocInfo.descriptorPool = descriptorPool;
+    allocInfo.descriptorSetCount = imageCount;
+    allocInfo.pSetLayouts = layouts.data();
+
+    descriptorSets.resize(imageCount);
+    vkAllocateDescriptorSets(device, &allocInfo, descriptorSets.data());
+
+    for (size_t i = 0; i < imageCount; i++) {
+        VkDescriptorBufferInfo bufferInfo{};
+        bufferInfo.buffer = uniformBuffers[i];
+        bufferInfo.offset = 0;
+        bufferInfo.range = sizeof(Transform);
+
+        VkWriteDescriptorSet descriptorWrite{};
+        descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+        descriptorWrite.dstSet = descriptorSets[i];
+        descriptorWrite.dstBinding = 0;
+        descriptorWrite.dstArrayElement = 0;
+        descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+        descriptorWrite.descriptorCount = 1;
+        descriptorWrite.pBufferInfo = &bufferInfo;
+        descriptorWrite.pImageInfo = nullptr;
+        descriptorWrite.pTexelBufferView = nullptr;
+
+        vkUpdateDescriptorSets(device, 1, &descriptorWrite, 0, nullptr);
+    }
+}
+
 void createCommandBuffers() {
     VkCommandBufferAllocateInfo allocateInfo{};
     allocateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
@@ -554,7 +665,10 @@ void createCommandBuffers() {
         vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
         vkCmdBindPipeline(commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
         vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
-        vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+        vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+        vkCmdBindDescriptorSets(commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout,
+                                0, 1, &descriptorSets[i], 0, nullptr);
+        vkCmdDrawIndexed(commandBuffers[i], static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
         vkCmdEndRenderPass(commandBuffers[i]);
         vkEndCommandBuffer(commandBuffers[i]);
     }
@@ -590,8 +704,34 @@ void setup() {
     createPipeline();
     createFramebuffers();
     createVertexBuffer();
+    createIndexBuffer();
+    createUniformBuffers();
+    createDescriptorPool();
+    createDescriptorSets();
     createCommandBuffers();
     createSyncObject();
+}
+
+void updateUniformBuffer(uint32_t imageIndex) {
+    static auto startTime = std::chrono::high_resolution_clock::now();
+    auto currentTime = std::chrono::high_resolution_clock::now();
+    float time = std::chrono::duration<float, std::chrono::seconds::period>(
+            currentTime - startTime).count();
+
+    Transform transform{};
+    transform.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f),
+                                  glm::vec3(0.0f, 0.0f, 1.0f));
+    transform.view = glm::lookAt(glm::vec3(0.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f),
+                                 glm::vec3(0.0f, 0.0f, 1.0f));
+    transform.proj = glm::perspective(glm::radians(45.0f), (float) swapchainExtent.width /
+                                                           (float) swapchainExtent.height, 0.1f,
+                                      10.0f);
+    transform.proj[1][1] *= -1;
+
+    void *data;
+    vkMapMemory(device, uniformMemory[imageIndex], 0, sizeof(Transform), 0, &data);
+    memcpy(data, &transform, sizeof(Transform));
+    vkUnmapMemory(device, uniformMemory[imageIndex]);
 }
 
 void draw() {
@@ -609,6 +749,8 @@ void draw() {
     VkSemaphore waitSemaphores[] = {availableSemaphores[currentImage]};
     VkSemaphore signalSemaphores[] = {finishedSemaphores[currentImage]};
     VkPipelineStageFlags waitStages[] = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
+
+    updateUniformBuffer(currentImage);
 
     VkSubmitInfo submitInfo{};
     submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
@@ -643,19 +785,27 @@ void clear() {
         vkDestroySemaphore(device, availableSemaphores[i], nullptr);
         vkDestroyFence(device, frameFences[i], nullptr);
     }
-    vkDestroyCommandPool(device, commandPool, nullptr);
+    vkDestroyDescriptorPool(device, descriptorPool, nullptr);
+    for (size_t i = 0; i < imageCount; i++) {
+        vkDestroyBuffer(device, uniformBuffers[i], nullptr);
+        vkFreeMemory(device, uniformMemory[i], nullptr);
+    }
+    vkDestroyBuffer(device, indexBuffer, nullptr);
+    vkFreeMemory(device, indexMemory, nullptr);
     vkDestroyBuffer(device, vertexBuffer, nullptr);
     vkFreeMemory(device, vertexMemory, nullptr);
     for (auto framebuffer : framebuffers)
         vkDestroyFramebuffer(device, framebuffer, nullptr);
     vkDestroyPipeline(device, graphicsPipeline, nullptr);
     vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+    vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
     vkDestroyRenderPass(device, renderPass, nullptr);
     vkDestroyShaderModule(device, fragmentShader, nullptr);
     vkDestroyShaderModule(device, vertexShader, nullptr);
     for (auto imageView : swapchainImageViews)
         vkDestroyImageView(device, imageView, nullptr);
     vkDestroySwapchainKHR(device, swapchain, nullptr);
+    vkDestroyCommandPool(device, commandPool, nullptr);
     vkDestroyDevice(device, nullptr);
     vkDestroySurfaceKHR(instance, surface, nullptr);
     auto destroyDebugUtilsMessenger = (PFN_vkDestroyDebugUtilsMessengerEXT)
@@ -681,7 +831,6 @@ extern "C" void android_main(struct android_app *pApp) {
 
     int events;
     android_poll_source *pSource;
-
 
     uint32_t previousFrame = 0, currentFrame = 0;
     uint64_t currentTime, previousTime = std::chrono::duration_cast<std::chrono::milliseconds>(
--- app/src/main/shaders/shader.vert
+++ app/src/main/shaders/shader.vert
@@ -1,12 +1,18 @@
 #version 460 core
 #extension GL_ARB_separate_shader_objects : enable
 
-layout(location = 0) in vec2 inPosition;
+layout(binding = 0) uniform Transform {
+    mat4 model;
+    mat4 view;
+    mat4 proj;
+} transform;
+
+layout(location = 0) in vec3 inPosition;
 layout(location = 1) in vec3 inColor;
 
 layout(location = 0) out vec3 fragColor;
 
 void main() {
-    gl_Position = vec4(inPosition, 0.0, 1.0);
+    gl_Position = transform.proj * transform.view * transform.model * vec4(inPosition, 1.0);
     fragColor = inColor;
 }
